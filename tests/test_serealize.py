e17b92a6091554577b9a0bfcfea95affaa08c4c2
KiryxaTech <kiryxatech@gmail.com>
2024-07-14 01:03:39 +0500

    Added serialization tests

---
+++ tests/test_serealize.py
@@ -0,0 +1,19 @@
+from ooj.json_serilizer import JsonSerializer
+
+class Person:
+    def __init__(self, name, age):
+        self.name = name
+        self.age = age
+
+    def get_values(self):
+        print(f"{self.name}, {self.age} yars old.")
+
+person = Person("Mike", 29)
+
+
+def test_create_serializer():
+    JsonSerializer({"ignore_errors": TypeError})
+
+def test_serialize():
+    serializer = JsonSerializer()
+    serializer.serialize(person)
