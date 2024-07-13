3e39ac450c0ef9105b623ed23fd1b7dbee1b186b
KiryxaTech <kiryxatech@gmail.com>
2024-07-14 01:50:51 +0500

    Added deserialize test

--- tests/test_serealize.py
+++ tests/test_serealize.py
@@ -12,8 +12,20 @@ person = Person("Mike", 29)
 
 
 def test_create_serializer():
-    JsonSerializer({"ignore_errors": TypeError})
+    serializer = JsonSerializer({"ignore_errors": TypeError})
+
+    assert serializer.get_serialization_options() == {"ignore_errors": TypeError}
 
 def test_serialize():
     serializer = JsonSerializer()
+    serialize_person = serializer.serialize(person)
+
+    assert serialize_person == '{"name": "Mike", "age": 29}'
+
+def test_deserialize():
+    serializer = JsonSerializer()
     serializer.serialize(person)
+
+    new_person: Person = serializer.deserialize(person, Person)
+
+    assert person.name == new_person.name
