a80e813f7bcc2605e5f75540dc3e70a39a4d582d
KiryxaTech <kiryxatech@gmail.com>
2024-07-13 23:27:12 +0500

    Added documentation to the JsonSerializer class, fixed a typo in the name of the serialize_to_file method

--- ooj/json_serilizer.py
+++ ooj/json_serilizer.py
@@ -1,10 +1,90 @@
 class JsonSerializer:
-    def __init__(self, options: dict = None) -> None: ...
-    def serialize(self, obj: object) -> str: ...
-    def deserialize(self, json_str: str, cls: type) -> object: ...
-    def serealize_to_file(self, obj: object, file_path: str): ...
-    def deserialize_from_file(self, file_path: str, cls: type) -> object: ...
-    def register_custom_type(self, cls: type, serializer: callable): ...
-    def handle_error(self, error: Exception): ...
-    def is_serializable(self, obj: object) -> dict: ...
-    def get_serialization_options(self) -> dict: ...
+    """A class for serializing and deserializing objects."""
+
+    def __init__(self, options: dict = None):
+        """
+        Initializing serialization settings.
+
+        Parameters:
+        - options (dict): Optional dictionary with settings.
+        """
+
+    def serialize(self, obj: object) -> str:
+        """
+        Converting an object to a JSON string.
+
+        Parameters:
+        - obj (object): Serializable object.
+
+        Returns:
+        - str: Serialized JSON string.
+        """
+    
+    def deserialize(self, json_str: str, cls: type) -> object:
+        """
+        Converts a JSON string into an object of the specified class.
+
+        Parameters:
+        - json_str (str): JSON string.
+        - cls (type): Target class type.
+
+        Returns:
+        - object: Deserialized object.
+        """
+    
+    def serialize_to_file(self, obj: object, file_path: str):
+        """
+        Saving a JSON representation of an object to a file.
+
+        Parameters:
+        - obj (object): Serializable object.
+        - file_path (str): Path to the file.
+        """
+
+    def deserialize_from_file(self, file_path: str, cls: type) -> object:
+        """
+        Loading an object from a JSON file.
+
+        Parameters:
+        - file_path (str): Path to the file.
+        - cls (type): Target class type.
+
+        Returns:
+        - object: Deserialized object.
+        """
+    
+    def register_custom_type(self, cls: type, serializer: callable):
+        """
+        Registers a custom type for specific serialization and deserialization.
+
+        Parameters:
+        - cls (type): Class type.
+        - serializer (callable): Custom serializer function.
+        """
+
+    def handle_error(self, error: Exception):
+        """
+        Handling and logging errors that occur during serialization and deserialization.
+
+        Parameters:
+        - error (Exception): Error to be handled.
+        """
+    
+    def is_serializable(self, obj: object) -> bool:
+        """
+        Checking if an object can be serialized.
+
+        Parameters:
+        - obj (object): Object to check.
+
+        Returns:
+        - bool: True if the object is serializable, False otherwise.
+        """
+
+    def get_serialization_options(self) -> dict:
+        """
+        Getting current serialization and deserialization options.
+
+        Returns:
+        - dict: Dictionary of options.
+        """
