f1fb25ca0c8cd04a6cb7a448dcbf8e36058ec89b
KiryxaTech <kiryxatech@gmail.com>
2024-07-14 02:54:20 +0500

    Added testing of serialize_to_file and deserialize_from_file methods

--- tests/__init__.py
+++ tests/__init__.py
@@ -1,4 +1,4 @@
 from .test_union import test_union_dict, test_union_file
 from .test_intersect import test_intersect_dict, test_intersect_file
 from .test_select import test_select
-from .test_serealize import test_create_serializer, test_serialize
+from .test_serealize import test_create_serializer, test_serialize, test_serialize_to_file
--- tests/test_serealize.py
+++ tests/test_serealize.py
@@ -1,4 +1,7 @@
+import json
+
 from ooj.json_serializer import JsonSerializer
+
 
 class Person:
     def __init__(self, name, age):
@@ -16,16 +19,39 @@ def test_create_serializer():
 
     assert serializer.get_serialization_options() == {"ignore_errors": TypeError}
 
+
 def test_serialize():
     serializer = JsonSerializer()
     serialize_person = serializer.serialize(person)
 
     assert serialize_person == '{"name": "Mike", "age": 29}'
 
+
 def test_deserialize():
     serializer = JsonSerializer()
     serializer.serialize(person)
 
-    new_person: Person = serializer.deserialize(person, Person)
+    deserialize_person: Person = serializer.deserialize(person, Person)
 
-    assert person.name == new_person.name
+    assert person.name == deserialize_person.name
+
+
+def test_serialize_to_file():
+    serialize_file_path = 'tests\\files\\serialize_obj.json'
+
+    serializer = JsonSerializer()
+    serializer.serialize_to_file(person, serialize_file_path)
+
+    with open(serialize_file_path, 'r', encoding='utf-8') as f:
+        serialize_data = json.load(f)
+
+    assert serialize_data == {"name": "Mike", "age": 29}
+
+
+def test_deserialize_from_file():
+    serialize_file_path = 'tests\\files\\serialize_obj.json'
+
+    serializer = JsonSerializer()
+    deserialize_person: Person = serializer.deserialize_from_file(serialize_file_path, Person)
+
+    assert person.age == deserialize_person.age
