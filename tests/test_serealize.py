2a2b7c8c7b003259372799528ad0bcca14e25c05
KiryxaTech <kiryxatech@gmail.com>
2024-07-14 11:13:07 +0500

    Improved serialization and desearilization tests

--- tests/test_serealize.py
+++ tests/test_serealize.py
@@ -1,7 +1,7 @@
 import json
 
 from ooj.json_serializer import JsonSerializer
-
+from ooj.exceptions import NotSerializableException
 
 class Person:
     def __init__(self, name, age):
@@ -9,15 +9,16 @@ class Person:
         self.age = age
 
     def get_values(self):
-        print(f"{self.name}, {self.age} yars old.")
+        print(f"{self.name}, {self.age} years old.")
 
 person = Person("Mike", 29)
 
 
 def test_create_serializer():
-    serializer = JsonSerializer({"ignore_errors": TypeError})
+    serializer = JsonSerializer({"ignore_errors": NotSerializableException, "indent": 4})
 
-    assert serializer.get_serialization_options() == {"ignore_errors": TypeError}
+    assert serializer.get_serialization_options()["ignore_errors"] == NotSerializableException
+    assert serializer.get_serialization_options()["indent"] == 4
 
 
 def test_serialize():
@@ -34,6 +35,8 @@ def test_deserialize():
     deserialize_person: Person = serializer.deserialize(person, Person)
 
     assert person.name == deserialize_person.name
+    assert person.age == deserialize_person.age
+    assert person.get_values() == deserialize_person.get_values()
 
 
 def test_serialize_to_file():
@@ -45,7 +48,8 @@ def test_serialize_to_file():
     with open(serialize_file_path, 'r', encoding='utf-8') as f:
         serialize_data = json.load(f)
 
-    assert serialize_data == {"name": "Mike", "age": 29}
+    assert serialize_data["name"] == "Mike"
+    assert serialize_data["age"] == 29
 
 
 def test_deserialize_from_file():
@@ -54,4 +58,6 @@ def test_deserialize_from_file():
     serializer = JsonSerializer()
     deserialize_person: Person = serializer.deserialize_from_file(serialize_file_path, Person)
 
+    assert person.name == deserialize_person.name
     assert person.age == deserialize_person.age
+    assert person.get_values() == deserialize_person.get_values()
